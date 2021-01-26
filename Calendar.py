197532871b177a407d858e87e64e06e6b5d82121
tonic523 <59171113+tonic523@users.noreply.github.com>
2021-01-26 18:04:45 +0900

    Create Calendar.py
    Print calendar format

---
+++ Calendar.py
@@ -0,0 +1,7 @@
+print('\t\t< 2021 1 >')
+print('Mo\tTu\tWe\tTh\tFr\tSa\tSu')
+print('1\t2\t3\t4\t5\t6\t7')
+print('8\t9\t10\t11\t12\t13\t14')
+print('15\t16\t17\t18\t19\t20\t21')
+print('22\t23\t24\t25\t26\t27\t28')
+print('29\t30\t31\tTh\tFr\tSa\tSu')
